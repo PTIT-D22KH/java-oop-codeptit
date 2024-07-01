048a0f3fd3263ad20b883b80672dd10c543fc4e7
ctduongvu <165153988+ctduongvu@users.noreply.github.com>
2024-07-01 10:10:40 +0000

    add files

--- submit/codeptit/src/j01003/j01003.java
+++ submit/codeptit/src/j01003/j01003.java
@@ -1,8 +1,15 @@
 package j01003;
-
+import java.util.*;
 public class j01003 {
+    public static Scanner input = new Scanner(System.in);
     public static void main(String[] args) {
         // Write your code here
-        
+        testCase();
+    }
+    public static void testCase() {
+        int a = input.nextInt();
+        int b = input.nextInt();
+        LinearEquation l = new LinearEquation(a, b);
+        l.solveEquation();
     }
 }
