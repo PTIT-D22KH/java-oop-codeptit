0ff647336b6644233d47798c664fb41e6a6f1840
ctduongvu <165153988+ctduongvu@users.noreply.github.com>
2024-07-01 09:18:32 +0000

    add files

--- submit/codeptit/src/j01001/j01001.java
+++ submit/codeptit/src/j01001/j01001.java
@@ -1,8 +1,16 @@
 package j01001;
-
+import java.util.*;
 public class j01001 {
     public static void main(String[] args) {
         // Write your code here
-        System.out.println("Hello PTIT.");
+        testCase();
+    }
+    public static void testCase() {
+        Scanner input = new Scanner(System.in);
+        int a = input.nextInt();
+        int b = input.nextInt();
+        Rectangle r = new Rectangle(a, b);
+        System.out.printf("%d %d", r.getParameter(), r.getArea());
+        input.close();
     }
 }
--- submit/codeptit/src/j01001/j01001_1.java
+++
@@ -1,8 +0,0 @@
-package j01001;
-
-public class j01001_1 {
-    public static void main(String[] args) {
-        // Write your code here
-        System.out.println("Hello PTIT.");
-    }
-}
