f11c09d97d2f5b49c51fa862441c7c5719ef792b
ctduongvu <165153988+ctduongvu@users.noreply.github.com>
2024-07-04 15:26:07 +0000

    add files

---
+++ submit/codeptit/src/j02106/j02106.java
@@ -0,0 +1,32 @@
+package j02106;
+import java.util.*;
+public class j02106 {
+    public static void main(String[] args) {
+        // Write your code here
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int [][]a = new int[n][n];
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+                a[i][j] = input.nextInt();
+            }
+        }
+        int res = 0;
+        for (int i = 0; i < n; i++ ){
+            int cnt0 = 0, cnt1 = 0;
+            for (int j = 0; j < n; j++) {
+                if (a[i][j] == 1) {
+                    cnt1++;
+                } else {
+                    cnt0++;
+                }
+            }
+            if (cnt1 > cnt0) {
+                res++;
+            }
+            
+        }
+        System.out.println(res);
+        input.close();
+    }
+}
