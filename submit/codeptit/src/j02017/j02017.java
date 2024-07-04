b8ce62299899cf27b27a56b84c66c6b88d4828dd
ctduongvu <165153988+ctduongvu@users.noreply.github.com>
2024-07-04 05:13:27 +0000

    add files

---
+++ submit/codeptit/src/j02017/j02017.java
@@ -0,0 +1,17 @@
+package j02017;
+import java.util.*;
+public class j02017 {
+    public static Scanner input  = new Scanner(System.in);
+    public static void main(String[] args) {
+        // Write your code here
+        testCase();
+    }
+    public static void testCase() {
+        int n = input.nextInt();
+        List<Integer> a = new ArrayList<Integer>();
+        for (int i = 0; i < n; i++) {
+            a.add(input.nextInt());
+        }
+        
+    }
+}
