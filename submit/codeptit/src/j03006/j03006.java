b29cff41050f2eec4531aa056e6d2f126f27fa64
ctduongvu <165153988+ctduongvu@users.noreply.github.com>
2024-07-05 01:04:55 +0000

    add files

---
+++ submit/codeptit/src/j03006/j03006.java
@@ -0,0 +1,12 @@
+package j03006;
+import java.util.*;
+public class j03006 {
+    public static Scanner input = new Scanner(System.in);
+    public static void main(String[] args) {
+        // Write your code here
+        int t = input.nextInt();
+        while (t-- > 0) {
+            
+        }
+    }
+}
