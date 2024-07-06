77e8781ce72052bac82fea12a2e1afba6ff50c1a
ctduongvu <165153988+ctduongvu@users.noreply.github.com>
2024-07-06 07:23:56 +0000

    add files

--- submit/codeptit/src/j03006/j03006.java
+++ submit/codeptit/src/j03006/j03006.java
@@ -6,7 +6,32 @@ public class j03006 {
         // Write your code here
         int t = input.nextInt();
         while (t-- > 0) {
-            
+            testCase();
+        }
+    }
+    public static boolean isPrettyNumber(String s) {
+        int n = s.length();
+        int l = 0, r = n - 1;
+        while (l <= r) {
+            if (s.charAt(l) != s.charAt(r)) {
+                return false;
+            }
+            int a = Integer.valueOf(s.charAt(l)) - '0';
+            int b = Integer.valueOf(s.charAt(r)) - '0';
+            if (a % 2 == 1 || b % 2 == 1) {
+                return false;
+            }
+            l++;
+            r--;
+        }
+        return true;
+    }
+    public static void testCase() {
+        String s = input.next();
+        if (isPrettyNumber(s)) {
+            System.out.println("YES");
+        } else {
+            System.out.println("NO");
         }
     }
 }
