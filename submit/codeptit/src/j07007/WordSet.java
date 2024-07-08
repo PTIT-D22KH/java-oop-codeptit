fcd8a15d64232247b50cdc750961671d9a217477
ctduongvu <165153988+ctduongvu@users.noreply.github.com>
2024-07-08 05:05:16 +0000

    add j07007

--- submit/codeptit/src/j07007/WordSet.java
+++ submit/codeptit/src/j07007/WordSet.java
@@ -1,11 +1,32 @@
 package j07007;
 
+import java.io.File;
 import java.io.FileNotFoundException;
+import java.util.Scanner;
 import java.util.TreeSet;
 
 public class WordSet {
     TreeSet<String> st = new TreeSet<>();
+    String showImage;
     public WordSet(String fileName) throws FileNotFoundException {
-        
+        Scanner input = new Scanner(new File(fileName));
+        while (input.hasNextLine()) {
+            String line = input.nextLine().toLowerCase();
+            String []a = line.split("\\s+");
+            for (String x : a) {
+                st.add(x);
+            }
+        }
+        input.close();
+        StringBuilder res = new StringBuilder("");
+        for (String s : st) {
+            res.append(s).append("\n");
+        }
+        showImage = res.toString().trim();
     }
+    @Override
+    public String toString() {
+        return showImage;
+    }
+    
 }
--- submit/codeptit/src/j07007/j07007.java
+++ submit/codeptit/src/j07007/j07007.java
@@ -1,7 +1,11 @@
 package j07007;
 
+import java.io.IOException;
+
 public class j07007 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
         // Write your code here
+        WordSet s = new WordSet("j07007/VANBAN.in");
+        System.out.println(s);
     }
 }
