53bc3b65569e48a5f8ccbdf906b8ec0e8e52fd23
ctduongvu <165153988+ctduongvu@users.noreply.github.com>
2024-07-08 03:15:03 +0000

    add files

---
+++ submit/codeptit/src/j07001/j07001.java
@@ -0,0 +1,15 @@
+package j07001;
+import java.util.*;
+import java.io.*;
+
+public class j07001 {
+    
+    public static void main(String[] args) throws IOException{
+        // Write your code here
+        Scanner input = new Scanner(new File("j07001/DATA.in"));
+        while (input.hasNextLine()) {
+            System.out.println(input.nextLine());
+        }
+        input.close();
+    }
+}
