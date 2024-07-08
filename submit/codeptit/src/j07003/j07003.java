5d0559e679657e93e9547df1d91d6dd59ce6e4f8
ctduongvu <165153988+ctduongvu@users.noreply.github.com>
2024-07-08 03:42:45 +0000

    add j07003

---
+++ submit/codeptit/src/j07003/j07003.java
@@ -0,0 +1,25 @@
+package j07003;
+import java.util.*;
+import java.io.*;
+import java.math.BigInteger;
+public class j07003 {
+    public static void main(String[] args) throws IOException{
+        // Write your code here
+        Scanner input = new Scanner(new File("j07003/DATA.in"));
+        String s = input.nextLine();
+        if (s.length() == 1) {
+            System.out.println(s);
+            input.close();
+            return;
+        }
+        while (s.length() > 1) {
+            BigInteger a = new BigInteger(s.substring(0, s.length() / 2));
+            BigInteger b = new BigInteger(s.substring(s.length() / 2));
+            s = a.add(b).toString();
+            System.out.println(s);
+        }
+
+        input.close();
+        
+    }
+}
