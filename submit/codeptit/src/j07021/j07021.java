466145ad394d59188883defcfb7ec04983663e01
ctduongvu <165153988+ctduongvu@users.noreply.github.com>
2024-07-08 08:02:17 +0000

    edit files

--- submit/codeptit/src/j07021/j07021.java
+++ submit/codeptit/src/j07021/j07021.java
@@ -1,7 +1,24 @@
 package j07021;
-
+import java.util.*;
+import java.io.*;
 public class j07021 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException{
         // Write your code here
+        // Scanner input = new Scanner(new File("j07021/DATA.in"));
+        Scanner input = new Scanner(new File("DATA.in"));
+        while (true) {
+            String s = input.nextLine().trim();
+            String []a = s.toLowerCase().split("\\s+");
+            if (s.equals("END")) {
+                break;
+            }     
+            for (int i = 0; i < a.length; i++) {
+                String tmp = String.valueOf(a[i].charAt(0)).toUpperCase() + a[i].substring(1);
+                System.out.printf("%s ", tmp);
+            }
+            System.out.println();
+            
+        }
+        input.close();
     }
 }
