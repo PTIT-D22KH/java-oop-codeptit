19b4bde1fe3c3a4011182adaa3e6bdef728c4045
ctduongvu <165153988+ctduongvu@users.noreply.github.com>
2024-07-08 05:09:02 +0000

    add files

--- submit/codeptit/src/j07007/j07007.java
+++ submit/codeptit/src/j07007/j07007.java
@@ -5,7 +5,8 @@ import java.io.IOException;
 public class j07007 {
     public static void main(String[] args) throws IOException {
         // Write your code here
-        WordSet s = new WordSet("j07007/VANBAN.in");
+        // WordSet s = new WordSet("j07007/VANBAN.in");
+        WordSet s = new WordSet("VANBAN.in");
         System.out.println(s);
     }
 }
