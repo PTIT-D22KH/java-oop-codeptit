3367c6140e706b6933ba44b214a704a92f99a75f
duongvu6 <vucongtuanduong6@gmail.com>
2024-07-15 10:33:51 +0000

    add files

--- submit/codeptit/src/j04002/j04002.java
+++ submit/codeptit/src/j04002/j04002.java
@@ -1,7 +1,18 @@
 package j04002;
 
+import java.util.Scanner;
+
 public class j04002 {
     public static void main(String[] args) {
         // Write your code here
+        Scanner input = new Scanner(System.in);
+        double height, width;
+        String color;
+        height = input.nextDouble();
+        width = input.nextDouble();
+        color = input.next();
+        Rectangle a = new Rectangle(height, width, color);
+        System.out.printf("%d %d %s", (int)a.findPerimeter(), (int)a.findArea(), Rectangle.getColor());
+        input.close();
     }
 }
