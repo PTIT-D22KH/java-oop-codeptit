ee7690a8522d0289f18609bab3f48125c4db541d
duongvu6 <vucongtuanduong6@gmail.com>
2024-07-19 07:56:30 +0000

    add j05011

---
+++ submit/codeptit/src/j05011/GamingTime.java
@@ -0,0 +1,5 @@
+package j05011;
+
+public class GamingTime {
+
+}
---
+++ submit/codeptit/src/j05011/j05011.java
@@ -0,0 +1,7 @@
+package j05011;
+
+public class j05011 {
+    public static void main(String[] args) {
+        // Write your code here
+    }
+}
