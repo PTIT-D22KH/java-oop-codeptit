aaea890ec7f8827d86e82a15e52746cf22fdfa6f
duongvu6 <vucongtuanduong6@gmail.com>
2024-07-19 09:21:26 +0000

    add j05012

---
+++ submit/codeptit/src/j05012/Bill.java
@@ -0,0 +1,5 @@
+package j05012;
+
+public class Bill {
+
+}
---
+++ submit/codeptit/src/j05012/j05012.java
@@ -0,0 +1,7 @@
+package j05012;
+
+public class j05012 {
+    public static void main(String[] args) {
+        // Write your code here
+    }
+}
