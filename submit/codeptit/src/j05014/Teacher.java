0c6cf3a8153a4db24989f17cbbf77cac3e171f55
duongvu6 <vucongtuanduong6@gmail.com>
2024-07-22 10:34:41 +0000

    add files

---
+++ submit/codeptit/src/j05014/Teacher.java
@@ -0,0 +1,13 @@
+package j05014;
+
+public class Teacher {
+    private String id, groupId, name, result;
+    private double m1, m2, totalScore;
+
+    public Teacher(String name, String groupId, double m1, double m2) {
+        this.name = name;
+        this.groupId = groupId;
+        this.m1 = m1;
+        this.m2 = m2;
+    }
+}
---
+++ submit/codeptit/src/j05014/j05014.java
@@ -0,0 +1,7 @@
+package j05014;
+
+public class j05014 {
+    public static void main(String[] args) {
+        // Write your code here
+    }
+}
