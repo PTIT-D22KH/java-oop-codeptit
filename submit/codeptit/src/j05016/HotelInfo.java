404de3ea8c60dc3de8d521556a68a34b7e18452b
duongvu6 <vucongtuanduong6@gmail.com>
2024-07-23 03:22:52 +0000

    add j05016

--- submit/codeptit/src/j05016/HotelInfo.java
+++ submit/codeptit/src/j05016/HotelInfo.java
@@ -1,7 +1,40 @@
 package j05016;
 
 public class HotelInfo {
-    private String roomId, floor;
-    private Date checkInDate, checkOutDate;
+    private String roomId;
+    private int floor;
+    private MyDate checkInDate, checkOutDate;
+    private long numDays;
+    private long additionalCost, totalCost, basicCost;
     
+
+    public HotelInfo(String roomId, String checkInString, String checkOutString, long additionalCost) {
+        this.roomId = roomId;
+        this.checkInDate = new MyDate(checkInString);
+        this.checkOutDate = new MyDate(checkOutString);
+        this.floor = Integer.parseInt(roomId.substring(0, 1));
+        this.additionalCost = additionalCost;
+        this.numDays = checkInDate.daysDiff(checkOutDate);
+        this.totalCost = calTotalCost();
+    }
+    private long calTotalCost() {
+        long res = 0;
+        if (floor == 1) {
+            res = 25 * this.numDays + additionalCost;
+        } else if (floor == 2) {
+            res = 34 * this.numDays + additionalCost;
+        } else if (floor == 3) {
+            res = 50 * this.numDays + additionalCost;
+        } else if (floor == 4) {
+            res = 80 * this.numDays + additionalCost;
+        }
+        return res;
+    }
+    public long getTotalCost() {
+        return totalCost;
+    }
+    @Override
+    public String toString(){ 
+        return roomId + " " + numDays + " " + totalCost;
+    }
 }
--- submit/codeptit/src/j05016/j05016.java
+++ submit/codeptit/src/j05016/j05016.java
@@ -1,7 +1,31 @@
 package j05016;
-
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
 public class j05016 {
     public static void main(String[] args) {
         // Write your code here
+        Scanner input = new Scanner(System.in);
+        int n = Integer.parseInt(input.nextLine());
+        ArrayList<Customer> a = new ArrayList<>(n);
+        for (int i = 0; i < n ;i++) {
+            String name, roomId, checkInString, checkOutString;
+            long additionalCost;
+            name = input.nextLine();
+            // System.out.println(name);
+            roomId = input.nextLine();
+            // System.out.println(roomId);
+            checkInString = input.nextLine();
+            // System.out.println(checkInString);
+            checkOutString = input.nextLine();
+            // System.out.println(checkOutString);
+            additionalCost = Long.parseLong(input.nextLine());
+            a.add(new Customer(name, roomId, checkInString, checkOutString, additionalCost));
+        }
+        Collections.sort(a, new CompareByTotalCost());
+        for (int i = 0; i < n; i++) {
+            System.out.println(a.get(i));
+        }
+        input.close();
     }
 }
