04e0900241560fc468f04c89fd2ac778d7c91552
tuanduongvu <vucongtuanduong249@gmail.com>
2024-07-29 11:30:25 +0000

    add j05030

---
+++ submit/codeptit/src/j05030/Student.java
@@ -0,0 +1,31 @@
+package j05030;
+
+import java.util.Comparator;
+
+public class Student {
+    private String id, name, className;
+    private double score1, score2, score3;
+    // private static int count = 0;
+    public Student(String id, String name, String className, double score1, double score2, double score3) {
+        this.id = id;
+        this.name = name;
+        this.className = className;
+        this.score1 = score1;
+        this.score2 = score2;
+        this.score3 = score3;
+
+    }
+    public String getId() {
+        return id;
+    }
+    @Override
+    public String toString(){ 
+        return id + " " + name + " " + className + " " + String.format("%.1f", score1) + " " + String.format("%.1f", score2) + " " + String.format("%.1f", score3);
+    }
+}
+class CompareById implements Comparator<Student> {
+    @Override
+    public int compare(Student a, Student b) {
+        return a.getId().compareTo(b.getId());
+    }
+}
