1b4f18b7d212316435d8da4b5ca1c1fb56238147
tuanduongvu <vucongtuanduong249@gmail.com>
2024-07-30 10:56:43 +0000

    edit files

--- submit/codeptit/src/j05036/Product.java
+++ submit/codeptit/src/j05036/Product.java
@@ -11,9 +11,21 @@ public class Product {
         this.unit = unit;
         this.buyPrice = buyPrice;
         this.quantity = quantity;
-        
+        count++;
+        this.id = String.format("MH%02d", count);
+
+    }
+    private double calShippingFee(double buyPrice, double quantity) {
+        return roundDouble(buyPrice * quantity * 0.05);
     }
 
+    private double roundDouble(double n) {
+        long x  = (long)n;
+        if (Math.abs(x - n) >= 0.5) {
+            return x + 1;
+        }
+        return x;
+    }
     
 
 }
