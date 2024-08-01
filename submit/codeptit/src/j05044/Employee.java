89ad1e9322682fd3822b1c6ee8bdf88ff242f82c
tuanduongvu <vucongtuanduong249@gmail.com>
2024-08-01 03:16:12 +0000

    add j05044

---
+++ submit/codeptit/src/j05044/Employee.java
@@ -0,0 +1,54 @@
+package j05044;
+
+public class Employee {
+    private String id, name, role;
+    private double basicSal, numDays, stipend, monthlySal, advancedSal, remainSal, totalSal;
+    private static int count = 0;
+    
+    public Employee(String name, String role, double basicSal, double numDays) {
+        this.name = name;
+        this.role = role;
+        this.basicSal = basicSal;
+        this.numDays = numDays;
+        count++;
+        this.id = String.format("NV%02d", count);
+        this.stipend = calStipend(role);
+        this.monthlySal = calMonthlySal();
+        this.totalSal = calTotalSal();
+        this.advancedSal = calAdvancedSal();
+        this.remainSal = this.totalSal - this.advancedSal;
+    }
+    private double calMonthlySal() {
+        return basicSal * numDays;
+    }
+    private double calStipend(String role) {
+        switch(role) {
+            case "GD" : 
+                return 500;
+            case "PGD":
+                return 400;
+            case "TP":
+                return 300;
+            case "KT":
+                return 250;
+            default:
+                return 100;
+        }
+    }
+    private double calTotalSal() {
+        return monthlySal+ stipend;
+    }
+    private double calAdvancedSal() {
+        if (totalSal * 2/3 < 25000) {
+            return Math.round(totalSal * 2 / 3 / 1000) * 1000;
+        } 
+        return 25000;
+    }
+    public String getRole() {
+        return role;
+    }
+    @Override
+    public String toString() {
+        return id + " " + name + " " + String.format("%.0f", stipend) + " " + String.format("%.0f", monthlySal) + " " + String.format("%.0f", advancedSal) + " " + String.format("%.0f", remainSal);
+    }
+}
