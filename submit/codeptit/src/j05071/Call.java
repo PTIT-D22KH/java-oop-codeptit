1e1e1b1bc9dc916d47c49f358f2c4162194ffcb5
tuanduongvu <vucongtuanduong249@gmail.com>
2024-08-02 05:25:22 +0000

    edit j05071

--- submit/codeptit/src/j05071/Call.java
+++ submit/codeptit/src/j05071/Call.java
@@ -1,5 +1,31 @@
 package j05071;
 
-public class Call {
+import java.util.HashMap;
+import java.util.Map;
 
+public class Call {
+    private String phone ;
+    private Time start, end;
+    private City city;
+    private long time, price;
+    private static Map<Integer, City> cityMap = new HashMap<>();
+    public Call(String phone, String start, String end) {
+        this.phone = phone;
+        this.start = new Time(start);
+        this.end = new Time(end);
+        // this.city = city;
+        this.time = this.end.timeToMinutes() - this.start.timeToMinutes();
+        if (phone.contains("-")) {
+            this.city = cityMap.get(Integer.valueOf(phone.substring(1, 3)));
+        } else {
+            if (this.time % 3 == 0) {
+                this.time /= 3;
+            } else {
+                this.time = this.time / 3 + 1;
+            }
+            this.price = 800 * this.time;
+        }
+    }
+
+    
 }
--- submit/codeptit/src/j05071/City.java
+++ submit/codeptit/src/j05071/City.java
@@ -1,5 +1,13 @@
 package j05071;
 
 public class City {
+    private String id, name;
+    private long price;
+    public City(String id, String name, long price) {
+        this.id = id;
+        this.name = name;
+        this.price = price;
+    }
 
+    
 }
