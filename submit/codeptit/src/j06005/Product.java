2354b73bbfd372186331d85992329463f6c03e6a
tuanduongvu <vucongtuanduong249@gmail.com>
2024-08-02 16:29:24 +0000

    add j06005

---
+++ submit/codeptit/src/j06005/Product.java
@@ -0,0 +1,40 @@
+package j06005;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Product {
+    private String id, name, unit;
+    private long buyPrice, sellPrice;
+    private static int count = 0;
+    private static Map<String, Product> productMap = new HashMap<>();
+    public Product(String name, String unit, long buyPrice, long sellPrice) {
+        this.name = name;
+        this.unit = unit;
+        this.buyPrice = buyPrice;
+        this.sellPrice = sellPrice;
+        count++;
+        this.id = String.format("MH%03d", count);
+        productMap.put(id, this);
+    }
+    public static Map<String, Product> getProductMap() {
+        return productMap;
+    }
+    public long getSellPrice() {
+        return sellPrice;
+    }
+    public long getBuyPrice() {
+        return buyPrice;
+    }
+    public String getName() {
+        return name;
+    }
+    public String getUnit() {
+        return unit;
+    }
+    @Override
+    public String toString(){ 
+        return name + " " + unit + " " + buyPrice + " " + sellPrice;
+    }
+    
+}
---
+++ submit/codeptit/src/j06005/j06005.java
@@ -0,0 +1,33 @@
+package j06005;
+
+import java.util.ArrayList;
+// import java.util.ArrayList;
+import java.util.Scanner;
+
+public class j06005 {
+    public static void main(String[] args) {
+        // Write your code here
+        Scanner input = new Scanner(System.in);
+        int n = Integer.parseInt(input.nextLine());
+        // ArrayList<Customer> customers = new ArrayList<>(n);
+
+        for (int i = 0; i < n; i++ ){
+            Customer a = new Customer(input.nextLine(), input.nextLine(), input.nextLine(), input.nextLine());
+        }
+        n = Integer.parseInt(input.nextLine());
+        for (int i = 0; i < n; i++ ){
+            Product a = new Product(input.nextLine(), input.nextLine(), Long.parseLong(input.nextLine()), Long.parseLong(input.nextLine()));
+        }
+        n = Integer.parseInt(input.nextLine());
+        ArrayList<Bill> bills = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) {
+            String s = input.nextLine();
+            String temp[] = s.split(" ");
+            bills.add(new Bill(temp[0], temp[1], Integer.parseInt(temp[2])));
+        }
+        for (Bill x : bills) {
+            System.out.println(x);
+        }
+        input.close();
+    }
+}
