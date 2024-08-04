8343799a03c3ce9e53d0069319bb6aba0559d5d7
tuanduongvu <vucongtuanduong249@gmail.com>
2024-08-04 16:15:49 +0000

    add j07012

---
+++ submit/codeptit/src/j07012/Word.java
@@ -0,0 +1,27 @@
+package j07012;
+
+import java.util.Comparator;
+
+public class Word {
+    private String word;
+    private int fre;
+    public Word(String word, int fre) {
+        this.word = word;
+        this.fre = fre;
+    }
+    public int getFre() {
+        return fre;
+    }
+
+    @Override
+    public String toString() {
+        return word + " " + fre;
+    }
+    
+}
+class CompareByFre implements Comparator<Word> {
+    @Override
+    public int compare(Word a, Word b) {
+        return Integer.compare(b.getFre(), a.getFre());
+    }
+}
