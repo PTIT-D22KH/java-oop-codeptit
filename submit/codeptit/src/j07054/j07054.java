f8d852adcb4dbd4e68ca3d5c0c8ccc488921b5e3
tuanduongvu <vucongtuanduong249@gmail.com>
2024-08-05 10:30:17 +0000

    add j07054

---
+++ submit/codeptit/src/j07054/j07054.java
@@ -0,0 +1,33 @@
+package j07054;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+
+public class j07054 {
+    public static void main(String[] args) throws IOException{
+        // Write your code here
+        // Scanner input = new Scanner(new File("j07054/BANGDIEM.in"));
+        Scanner input = new Scanner(new File("jBANGDIEM.in"));
+        int n = Integer.parseInt(input.nextLine());
+        ArrayList<Student> students = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) {
+            students.add(new Student(input.nextLine(), Double.parseDouble(input.nextLine()), Double.parseDouble(input.nextLine()), Double.parseDouble(input.nextLine())));
+        }
+        Collections.sort(students, new CompareByAverageScoreDesc());
+        int rank = 1;
+        for (int i = 0; i < n; i++) {
+            if (i == 0 || students.get(i).getAverageScore() != students.get(i - 1).getAverageScore()) {
+                rank = i + 1;
+            }
+            students.get(i).setRank(rank);
+        }
+        for (Student x : students ) {
+            System.out.println(x);
+        }
+        input.close();
+    }
+}
