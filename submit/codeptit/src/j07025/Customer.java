e1f651924c90b302d23b96bb399fff1002222750
tuanduongvu <vucongtuanduong249@gmail.com>
2024-08-05 07:14:09 +0000

    edit j07025

--- submit/codeptit/src/j07025/Customer.java
+++ submit/codeptit/src/j07025/Customer.java
@@ -1,42 +1,61 @@
 package j07025;
 
+import java.util.Date;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Comparator;
+
 public class Customer {
-    private String id, name, gender, dob, address;
+    private String id, name, gender, address, birthDay;
+    private Date dob;
     private static int count = 0;
 
-    public Customer(String name, String gender, String dob, String address) {
+    public Customer(String name, String gender, String birthDay, String address) throws ParseException {
         count++;
         this.id = String.format("KH%03d", count);
         this.name = formatName(name);
         this.gender = gender;
-        this.dob = formatDob(dob);
+        this.birthDay = formatDob(birthDay);
         this.address = address;
     }
     private String formatName(String name) {
         String a[] = name.split(" ");
-        String res = "";
+        StringBuilder res = new StringBuilder();
         for (int i = 0; i < a.length; i++) {
-            res += String.valueOf(a[i].toUpperCase().charAt(0)) + a[i].substring(1)
+            res.append(Character.toUpperCase(a[i].charAt(0))).append(a[i].substring(1).toLowerCase());
+            // res += String.valueOf(a[i].toUpperCase().charAt(0)) + a[i].substring(1).toLowerCase();
             if (i != a.length - 1) {
-                res += " ";
+                // res += " ";
+                res.append(" ");
             }
         }
-        return res;
+        return res.toString();
     }
 
-    private String formatDob(String dob) {
-        String res = dob;
-        if (dob.charAt(2) != '/') {
-            res = "0" + res;
-        }
-        if (dob.charAt(5) != ) {
-            res = res.substring(0, 3) + "0" + res.substring(3);
-        }
-        return res;
+    private String formatDob(String birthDay) throws ParseException{
+        String dateFormat = "dd/MM/yyyy";
+        SimpleDateFormat form = new SimpleDateFormat(dateFormat);
+        this.dob = form.parse(birthDay);
+        return form.format(this.dob);
+        
+    }
+    public Date getDob() {
+        return dob;
     }
     @Override
     public String toString() {
-        return id + " " + name + " " + gender + " " + address + " " + dob;
+        return id + " " + name + " " + gender + " " + address + " " + birthDay;
     }
     
+}
+class CompareByAge implements Comparator<Customer> {
+    @Override
+    public int compare(Customer a, Customer b) {
+        if (a.getDob().after(b.getDob())) {
+            return 1;
+            
+        }
+        return -1;
+    }
+
 }
--- submit/codeptit/src/j07025/j07025.java
+++ submit/codeptit/src/j07025/j07025.java
@@ -1,7 +1,26 @@
 package j07025;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.ParseException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
 public class j07025 {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, ParseException{
         // Write your code here
+        // Scanner input = new Scanner(new File("j07025/KHACHHANG.in"));
+        Scanner input = new Scanner(new File("KHACHHANG.in"));
+        ArrayList<Customer> customers = new ArrayList<>();
+        int n = Integer.parseInt(input.nextLine());
+        for (int i = 0; i < n; i++) {
+            customers.add(new Customer(input.nextLine(), input.nextLine(), input.nextLine(), input.nextLine()));
+        }
+        Collections.sort(customers, new CompareByAge());
+        for (Customer x : customers) {
+            System.out.println(x);
+        }
+        input.close();
     }
 }
