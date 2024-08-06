2eed02ff366c2a973d1242c73bd4ff74e2232413
tuanduongvu <vucongtuanduong249@gmail.com>
2024-08-06 01:39:37 +0000

    add j07055

---
+++ submit/codeptit/src/j07055/j07055.java
@@ -0,0 +1,25 @@
+package j07055;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class j07055 {
+    public static void main(String[] args) throws IOException{
+        // Write your code here
+        // Scanner input = new Scanner(new File("j07055/BANGDIEM.in"));
+        Scanner input = new Scanner(new File("BANGDIEM.in"));
+        int n = Integer.parseInt(input.nextLine());
+        ArrayList<Student> students = new ArrayList<>(n);
+        for (int i = 0; i < n; i++) {
+            students.add(new Student(input.nextLine(), Double.parseDouble(input.nextLine()), Double.parseDouble(input.nextLine()), Double.parseDouble(input.nextLine())));
+        }
+        Collections.sort(students, new CompareByFinalScoreDesc());
+        for (Student x : students ) {
+            System.out.println(x);
+        }
+        input.close();
+    }
+}
