841985b1c349108e73538db936a1098a452f49bc
duongvu5 <vucongtuanduong5@gmail.com>
2024-08-24 00:56:24 +0000

    add files

---
+++ classwork/classwork/src/ArrayLearn/ArrayLearn.java
@@ -0,0 +1,9 @@
+package ArrayLearn;
+import java.util.Arrays;
+public class ArrayLearn {
+    public static void main(String[] args) {
+        int a[] = new int[10];
+        Arrays.fill(a, 8);
+        System.out.println(Arrays.toString(a));
+    }
+}
