a44b30ef0eb8a9ee69b83bb0618ae20ffb831208
duongvu5 <vucongtuanduong5@gmail.com>
2024-08-24 01:25:02 +0000

    add files

--- classwork/classwork/src/StringLearn/README.md
+++ classwork/classwork/src/StringLearn/README.md
@@ -1,4 +1,8 @@
+
 ## StringLearn
-- [StringLearn.java](StringLearn.java)
+
 - [input.txt](input.txt)
 - [output.txt](output.txt)
+- [README.md](README.md)
+- [StringLearn.class](StringLearn.class)
+- [StringLearn.java](StringLearn.java)
--- classwork/classwork/src/StringLearn/StringLearn.java
+++ classwork/classwork/src/StringLearn/StringLearn.java
@@ -5,5 +5,13 @@ public class StringLearn {
         // Write your code here
         //string can not be changed after initialized
         
+        String s = "Hello World!";
+        System.out.println(s.charAt(0));
+        System.out.println(s.indexOf('W'));
+        System.out.println(s.length());
+        System.out.println(s.substring(1, 4));
+        System.out.println(s.toLowerCase());
+        System.out.println(s.toUpperCase());
+        System.out.println(s);
     }
 }
