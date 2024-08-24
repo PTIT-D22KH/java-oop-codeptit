2258cee6e81073d59d0df62583aee83815301cd4
duongvu5 <vucongtuanduong5@gmail.com>
2024-08-24 01:27:28 +0000

    add files

--- classwork/classwork/src/StringLearn/StringLearn.java
+++ classwork/classwork/src/StringLearn/StringLearn.java
@@ -7,11 +7,21 @@ public class StringLearn {
         
         String s = "Hello World!";
         System.out.println(s.charAt(0));
-        System.out.println(s.indexOf('W'));
+        System.out.println(s.indexOf("llo"));
         System.out.println(s.length());
         System.out.println(s.substring(1, 4));
         System.out.println(s.toLowerCase());
         System.out.println(s.toUpperCase());
         System.out.println(s);
+
+        String x = "Hello World!";
+        String y = "MyOwnString";
+
+        if (s.equals(x)) {
+            System.out.println("s equals x");
+        } 
+        if (s.equals(y)) {
+            System.out.println("s equals y");
+        }
     }
 }
