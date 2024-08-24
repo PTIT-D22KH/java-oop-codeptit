80666060e87b0f722458e9eea5447fd220f63cd6
duongvu5 <vucongtuanduong5@gmail.com>
2024-08-24 01:13:27 +0000

    add files

---
+++ classwork/classwork/src/EnumLearn/EnumLearn.java
@@ -0,0 +1,8 @@
+package EnumLearn;
+
+public class EnumLearn {
+    public static void main(String[] args) {
+        // Write your code here
+        public enum Monster(ZOMBIE, VAMPIRE, DEMON, WEREWOLF);
+    }
+}
---
+++ classwork/classwork/src/EnumLearn/README.md
@@ -0,0 +1,4 @@
+## EnumLearn
+- [EnumLearn.java](EnumLearn.java)
+- [input.txt](input.txt)
+- [output.txt](output.txt)
