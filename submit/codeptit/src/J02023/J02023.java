398272b13d4b75ce8ac7913864086988db32152a
ctduongvu <165153988+ctduongvu@users.noreply.github.com>
2024-08-31 04:01:28 +0000

    add files

--- submit/codeptit/src/J02023/J02023.java
+++ submit/codeptit/src/J02023/J02023.java
@@ -4,23 +4,27 @@ import java.util.Scanner;
 
 public class J02023 {
     public static void main(String[] args) {
-        // Write your code here
         Scanner input = new Scanner(System.in);
         int n = input.nextInt();
         int s = input.nextInt();
         input.close();
-        if ((s < 1) || s > 9 * n) {
+
+        if ((s < 1 && n > 1) || s > 9 * n) {
             System.out.println("-1 -1");
             return;
         }
+
         if (s == 0 && n == 1) {
             System.out.println("0 0");
             return;
-        } 
+        }
+
         int sum = s;
-    
+        StringBuilder smallest = new StringBuilder();
         StringBuilder largest = new StringBuilder();
-        while (n > 0) {
+
+        // Construct the largest number
+        for (int i = 0; i < n; i++) {
             if (sum > 9) {
                 largest.append("9");
                 sum -= 9;
@@ -28,20 +32,21 @@ public class J02023 {
                 largest.append(String.valueOf(sum));
                 sum = 0;
             }
-            n--;
         }
-        String smallest = largest.toString();
-        StringBuilder smallestBuilder = new StringBuilder(smallest);
-        smallestBuilder.reverse();
-        if (smallest.charAt(0) == '0') {
-            int i = 1;
-            smallestBuilder.setCharAt(0, '1');
-            while (smallest.charAt(0) == '0') {
-                i++;
-            }
-            smallestBuilder.setCharAt(i, (char) (smallest.charAt(i) - 1));
-        }
-        System.out.println(smallestBuilder.toString() + " " + largest.toString());
 
+        // Construct the smallest number
+        sum = s;
+        for (int i = 0; i < n; i++) {
+            for (int d = (i == 0 ? 1 : 0); d <= 9; d++) {
+                if (sum - d <= 9 * (n - i - 1)) {
+                    smallest.append(d);
+                    sum -= d;
+                    break;
+                }
+            }
+        }
+
+        // Print the results
+        System.out.println(smallest.toString() + " " + largest.toString());
     }
 }
--- submit/codeptit/src/J02023/README.md
+++ submit/codeptit/src/J02023/README.md
@@ -1,6 +1,8 @@
 
 ## J02023
 ![alt text](image.png)
+
+- [image.png](image.png)
 - [input.txt](input.txt)
 - [J02023.class](J02023.class)
 - [J02023.java](J02023.java)
