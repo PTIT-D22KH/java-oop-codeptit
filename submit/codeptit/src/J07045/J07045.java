665aaee1ec91b5f25a89208049dbf8ce36b34c6c
ctduongvu <165153988+ctduongvu@users.noreply.github.com>
2024-09-01 16:29:12 +0000

    update files

--- submit/codeptit/src/J07045/J07045.java
+++ submit/codeptit/src/J07045/J07045.java
@@ -3,25 +3,20 @@ package J07045;
 import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.Scanner;
 import java.util.*;
 
 public class J07045 {
-    public static void main(String[] args) throws FileNotFoundException, IOException{
-        // Write your code here
-        Scanner input = new Scanner(new File("J07045/PHONG.in"));
-
-        int n = Integer.parseInt(input.nextLine());
-        ArrayList<Room> a = new ArrayList<>(n);
-        for (int i = 0; i < n; i++) {
-            String s = input.nextLine();
-            String temp[] = s.trim().split("\\s+");
-            a.add(new Room(temp[0], temp[1], Double.parseDouble(temp[2]), Double.parseDouble(temp[3])));
+    public static void main(String[] args) throws IOException {
+        ArrayList<LoaiPhong> ds = new ArrayList<>();
+        Scanner in = new Scanner(new File("PHONG.in"));
+        // Scanner in = new Scanner(new File("J07045/PHONG.in"));
+        int n = Integer.parseInt(in.nextLine());
+        while(n-->0){
+            ds.add(new LoaiPhong(in.nextLine()));
         }
-        Collections.sort(a, new CompareByName());
-        for (Room x : a) {
-            System.out.println(x);
+        Collections.sort(ds);
+        for(LoaiPhong tmp : ds){
+            System.out.println(tmp);
         }
     }
 }
--- submit/codeptit/src/J07045/README.md
+++ submit/codeptit/src/J07045/README.md
@@ -1,9 +1,14 @@
 
 ## J07045
 ![alt text](image.png)
+
+- [CompareByName.class](CompareByName.class)
+- [image.png](image.png)
 - [input.txt](input.txt)
+- [J07045.class](J07045.class)
 - [J07045.java](J07045.java)
+- [LoaiPhong.java](LoaiPhong.java)
 - [output.txt](output.txt)
 - [PHONG.in](PHONG.in)
 - [README.md](README.md)
-- [Room.java](Room.java)
+- [Room.class](Room.class)
