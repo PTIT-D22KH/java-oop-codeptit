e327617c42c806454fbbc06d2f04e0bf2b37e4e1
ctduongvu <165153988+ctduongvu@users.noreply.github.com>
2024-09-03 10:00:23 +0000

    add files

--- submit/codeptit/src/J04021/IntSet.java
+++ submit/codeptit/src/J04021/IntSet.java
@@ -3,6 +3,7 @@ package J04021;
 import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.TreeSet;
 
 public class IntSet {
     private Set<Integer> se = new HashSet<>();
@@ -17,12 +18,16 @@ public class IntSet {
     public IntSet union(IntSet other) {
         Set<Integer> res = new HashSet<>();
         res.addAll(se);
-        res.retainAll(other.se);
+        res.addAll(other.se);
         return new IntSet(res);
     }
     @Override
     public String toString() {
         StringBuilder res = new StringBuilder();
-        
+        Set<Integer> sortedSet = new TreeSet<>(se);
+        for (int x : sortedSet) {
+            res.append(x).append(" ");
+        }
+        return res.toString().trim();
     }
 }
