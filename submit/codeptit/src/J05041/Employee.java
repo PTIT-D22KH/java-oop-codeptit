8f6fed314053f6b6b9d512add00e6783424ab982
ctduongvu <165153988+ctduongvu@users.noreply.github.com>
2024-09-03 10:18:17 +0000

    add files

---
+++ submit/codeptit/src/J05041/Employee.java
@@ -0,0 +1,63 @@
+package J05041;
+
+import java.util.Comparator;
+
+public class Employee {
+    private String id, name, role;
+    private static int count = 0;
+    private long numDays, basicSal, reward, stipend, monthlySal, totalSal;
+    public Employee(String name, long basicSal,long numDays, String role) {
+        this.name = name;
+        count++;
+        this.id = String.format("NV%02d", count);
+        this.role = role;
+        this.numDays = numDays;
+        this.basicSal = basicSal;
+        this.monthlySal = calMonthlySal();
+        this.reward = calReward();
+        this.stipend = calStipend();
+        
+        this.totalSal = calTotalSal(); 
+    }
+
+    private long calMonthlySal() {
+        return basicSal * numDays;
+    }
+
+    private long calReward(){ 
+        if (numDays >= 25) {
+            return (long)(monthlySal * 0.2);
+        } else if (numDays >= 22) {
+            return (long)(monthlySal * 0.1);
+        }
+        return 0;
+    }
+    private long calStipend() {
+        if (role.equals("GD")) {
+            return 250000;
+        } else if (role.equals("PGD")) {
+            return 200000;
+        } else if (role.equals("TP")) {
+            return 180000;
+        } 
+        return 150000;
+    }
+    private long calTotalSal() {
+        return monthlySal + stipend + reward;
+    }
+    public long getTotalSal() {
+        return totalSal;
+    }
+    @Override
+    public String toString(){ 
+        return id + " " + name + " " + monthlySal + " " + reward + " " + stipend + " " + totalSal;
+    }
+
+    
+}
+class CompareByTotalSal implements Comparator<Employee> {
+    @Override
+    public int compare(Employee a, Employee b) {
+        return Long.compare(b.getTotalSal(), a.getTotalSal());
+    }
+}
---
+++ submit/codeptit/src/J05041/README.md
@@ -0,0 +1,9 @@
+
+## J05041
+![alt text](image.png)
+![alt text](image-1.png)
+- [Employee.java](Employee.java)
+- [input.txt](input.txt)
+- [J05041.java](J05041.java)
+- [output.txt](output.txt)
+- [README.md](README.md)
