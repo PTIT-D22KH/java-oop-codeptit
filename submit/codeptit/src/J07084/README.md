90da3577a6907593c034165f4074b5257cb88db8
ctduongvu <165153988+ctduongvu@users.noreply.github.com>
2024-09-03 07:13:12 +0000

    add files

--- submit/codeptit/src/J07084/README.md
+++ submit/codeptit/src/J07084/README.md
@@ -1,4 +1,9 @@
+
 ## J07084
-- [J07084.java](J07084.java)
+![alt text](image.png)
 - [input.txt](input.txt)
+- [J07084.java](J07084.java)
+- [ONLINE.in](ONLINE.in)
 - [output.txt](output.txt)
+- [README.md](README.md)
+- [Student.java](Student.java)
--- submit/codeptit/src/J07084/Student.java
+++ submit/codeptit/src/J07084/Student.java
@@ -1,6 +1,43 @@
 package J07084;
 
+import java.text.SimpleDateFormat;
+import java.util.Comparator;
+
 public class Student {
-    private String name, startTime, endTime;
+    private String name;
     private long duration;
+    public Student(String name, String startTime, String endTime) {
+        this.name = name;
+        SimpleDateFormat sd = new SimpleDateFormat("dd/MM/yyyy HH:mm:ss");
+        try {
+            long start = sd.parse(startTime).getTime();
+            long end = sd.parse(endTime).getTime();
+            this.duration = (end - start) / (1000 * 60);
+        } catch (Exception e) {
+            // TODO: handle exception
+            e.printStackTrace();
+        }
+    }
+
+    public long getDuration() {
+        return duration;
+    }
+    public String getName() {
+        return name;
+    }
+    @Override
+    public String toString() {
+        return name + " " + duration;
+    }
+    
+}
+class CompareByDurationAndName implements Comparator<Student> {
+    @Override
+    public int compare(Student a, Student b) {
+        int cmpDuration = Long.compare(b.getDuration(), a.getDuration());
+        if (cmpDuration == 0) {
+            return a.getName().compareTo(b.getName());
+        }
+        return cmpDuration;
+    }
 }
