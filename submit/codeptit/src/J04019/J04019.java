6d5f8a01acfb3f1baefd494b072dd9e56cdb1e59
ctduongvu <165153988+ctduongvu@users.noreply.github.com>
2024-09-03 10:02:44 +0000

    update files

--- submit/codeptit/src/J04019/J04019.java
+++ submit/codeptit/src/J04019/J04019.java
@@ -6,13 +6,14 @@ public class J04019 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int t = sc.nextInt();
-        while(t-- >0){
+        while (t-- > 0) {
             Triangle a = new Triangle(Point.nextPoint(sc), Point.nextPoint(sc), Point.nextPoint(sc));
-            if(!a.valid()){
+            if (!a.valid()) {
                 System.out.println("INVALID");
-            } else{
+            } else {
                 System.out.println(a.getPerimeter());
             }
         }
+        sc.close();
     }
 }
