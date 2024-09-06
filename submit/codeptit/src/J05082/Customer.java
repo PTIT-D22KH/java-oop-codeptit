da534231c6195bfb7eb2650e9648ea222b0cd15d
ctduongvu <165153988+ctduongvu@users.noreply.github.com>
2024-09-06 10:17:47 +0000

    update

--- submit/codeptit/src/J05082/Customer.java
+++ submit/codeptit/src/J05082/Customer.java
@@ -1,13 +1,10 @@
 package J05082;
-
 import java.text.SimpleDateFormat;
 import java.util.*;
-
 public class Customer {
     private String id, name, gender, dob, address;
     private Date birthDay;
     private static int count = 0;
-
     public Customer(String name, String gender, String dob, String address) {
         this.name = formatName(name);
         this.gender = gender;
@@ -17,27 +14,27 @@ public class Customer {
         this.id = String.format("KH%03d", count);
         SimpleDateFormat sd = new SimpleDateFormat("dd/MM/yyyy");
         try {
-            this.birthDay = sd.parse(this.dob);
+            this.birthDay = sd.parse(dob);
         } catch (Exception e) {
+            // TODO: handle exception
             e.printStackTrace();
         }
     }
-
     private String formatName(String name) {
-        String[] a = name.trim().toLowerCase().split("\\s+");
+        String a[] = name.trim().toLowerCase().split("\\s+");
         StringBuilder res = new StringBuilder();
-        for (String s : a) {
-            res.append(Character.toUpperCase(s.charAt(0))).append(s.substring(1)).append(" ");
+        for (int i = 0; i < a.length; i++) {
+            res.append(Character.toUpperCase(a[i].charAt(0))).append(a[i].substring(1)).append(" ");
         }
         return res.toString().trim();
     }
-
+    
     public Date getBirthDay() {
         return birthDay;
     }
 
-    private String normalizeDob(String dob) {
-        String[] a = dob.trim().split("/");
+    private String normalizeDob(String dob)  {
+        String a[] = dob.trim().split("/");
         StringBuilder res = new StringBuilder();
         for (int i = 0; i < a.length - 1; i++) {
             if (a[i].length() == 1) {
@@ -50,13 +47,13 @@ public class Customer {
         res.append(a[a.length - 1]);
         return res.toString();
     }
-
     @Override
     public String toString() {
         return id + " " + name + " " + gender + " " + address + " " + dob;
     }
-}
 
+    
+}
 class CompareByAge implements Comparator<Customer> {
     @Override
     public int compare(Customer a, Customer b) {
--- submit/codeptit/src/J07009/IntSet.java
+++ submit/codeptit/src/J07009/IntSet.java
@@ -5,5 +5,9 @@ import java.util.TreeSet;
 
 public class IntSet {
     private Set<Integer> se = new TreeSet<>();
-    
+    public IntSet(int a[]) {
+        for (int i = 0; i < a.length; i++) {
+            
+        }
+    }
 }
