7d305f917eedad5171be5340203e773d206d88b6
ctduongvu <165153988+ctduongvu@users.noreply.github.com>
2024-09-06 15:25:46 +0000

    add files

--- submit/codeptit/src/J07038/J07038.java
+++ submit/codeptit/src/J07038/J07038.java
@@ -1,56 +1,59 @@
 package J07038;
 import java.util.*;
 import java.io.*;
+
 public class J07038 {
-    public static void main(String[] args) throws IOException{
-        // Write your code here
-        // Scanner input1 = new Scanner(new File("J07038/SINHVIEN.in"));
-        // Scanner input2 = new Scanner(new File("J07038/DN.in"));
-        // Scanner input3 = new Scanner(new File("J07038/THUCTAP.in"));
-        
+    public static void main(String[] args) throws IOException {
         Scanner input1 = new Scanner(new File("SINHVIEN.in"));
         Scanner input2 = new Scanner(new File("DN.in"));
         Scanner input3 = new Scanner(new File("THUCTAP.in"));
 
         int n = Integer.parseInt(input1.nextLine());
-        ArrayList<Student> a = new ArrayList<>();
+        ArrayList<Student> students = new ArrayList<>();
         for (int i = 0; i < n; i++) {
-            a.add(new Student(input1.nextLine(), input1.nextLine(), input1.nextLine(), input1.nextLine()));
+            students.add(new Student(input1.nextLine(), input1.nextLine(), input1.nextLine(), input1.nextLine()));
         }
-        int m = Integer.parseInt(input2.nextLine());
-        ArrayList<Company> b = new ArrayList<>();
-        for (int i = 0; i < m; i++) {
-            b.add(new Company(input2.nextLine(), input2.nextLine(), Long.parseLong(input2.nextLine())));
 
+        int m = Integer.parseInt(input2.nextLine());
+        ArrayList<Company> companies = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            companies.add(new Company(input2.nextLine(), input2.nextLine(), Long.parseLong(input2.nextLine())));
         }
+
         int p = Integer.parseInt(input3.nextLine());
         for (int i = 0; i < p; i++) {
             String s = input3.nextLine().trim();
-            String temp[] = s.split("\\s+");
-            for (Student x : a) {
-                if (x.getId().equals(temp[0])) {
-                    x.setInternCompany(Company.getCompanyMap().get(temp[1]));
+            String[] temp = s.split("\\s+");
+            for (Student student : students) {
+                if (student.getId().equals(temp[0])) {
+                    student.setInternCompany(Company.getCompanyMap().get(temp[1]));
                     break;
                 }
             }
         }
+
         int k = Integer.parseInt(input3.nextLine());
-        for (int i = 0; i < k; i++)  {
+        for (int i = 0; i < k; i++) {
             String companyId = input3.nextLine();
             System.out.printf("DANH SACH THUC TAP TAI %s:\n", Company.getCompanyMap().get(companyId).getName());
+            List<Student> interns = new ArrayList<>();
+            for (Student student : students) {
+                if (student.getInternCompany() != null && student.getInternCompany().getId().equals(companyId)) {
+                    interns.add(student);
+                }
+            }
+            Collections.sort(interns, new CompareById());
             long count = 0;
-            for (Student x : a) {
-                if (x.getInternCompany().getId().equals(companyId) && count <= x.getInternCompany().getNumStudents()) {
-                    System.out.println(x);
+            for (Student intern : interns) {
+                if (count < intern.getInternCompany().getNumStudents()) {
+                    System.out.println(intern);
                     count++;
                 }
             }
         }
+
         input1.close();
         input2.close();
         input3.close();
-
-
-
     }
 }
---
+++ submit/codeptit/src/J07074/Section.java
@@ -0,0 +1,5 @@
+package J07074;
+
+public class Section {
+    private String groupId, courseId;
+}
