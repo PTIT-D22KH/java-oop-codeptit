e7e247171072ef7d6810c8bc6a45f17c93813fb4
ctduongvu <165153988+ctduongvu@users.noreply.github.com>
2024-09-07 04:40:56 +0000

    update files

---
+++ submit/codeptit/src/J05066/J05066.java
@@ -0,0 +1,26 @@
+package J05066;
+import java.util.*;
+public class J05066 {
+    public static void main(String[] args) {
+        // Write your code here
+        Scanner input = new Scanner(System.in);
+        int n = Integer.parseInt(input.nextLine());
+        ArrayList<Employee> a = new ArrayList<>();
+
+        for (int i = 0; i < n; i++) {
+            a.add(new Employee(input.nextLine()));
+        }
+        Collections.sort(a, new CompareByCoefficientAndStt());
+        int q = Integer.parseInt(input.nextLine());
+        for (int i = 0; i < q; i++) {
+
+            String role = input.nextLine();
+            for (Employee x : a) {
+                if (x.getRole().equals(role)) {
+                    System.out.println(x);
+                }
+            }
+            System.out.println();
+        }
+    }
+}
---
+++ submit/codeptit/src/J05066/README.md
@@ -0,0 +1,4 @@
+## J05066
+- [J05066.java](J05066.java)
+- [input.txt](input.txt)
+- [output.txt](output.txt)
