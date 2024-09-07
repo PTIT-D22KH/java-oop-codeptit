0b8ea944b1e775756ef7d3c3014b29650820f9d6
ctduongvu <165153988+ctduongvu@users.noreply.github.com>
2024-09-07 06:34:12 +0000

    update

--- submit/codeptit/src/J05073/Product.java
+++ submit/codeptit/src/J05073/Product.java
@@ -1,10 +1,16 @@
 package J05073;
 
 public class Product {
-    private String id, firstId, lastId;
+    private String firstId, lastId;
     private double tax, shippingFee;
 
     private long singlePrice;
 
-    
+    public Product(String billId, long singlePrice) {
+        this.singlePrice = singlePrice;
+        this.firstId = billId.substring(0, 1);
+        this.lastId = billId.substring(3);
+        preSetTaxAndShippingFee();
+    }
+
 }
