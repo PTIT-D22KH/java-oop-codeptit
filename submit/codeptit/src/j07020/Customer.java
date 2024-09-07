b1354e4124e01683f1bb65d4de5935ad79186f36
ctduongvu <165153988+ctduongvu@users.noreply.github.com>
2024-09-07 02:08:44 +0000

    update

--- submit/codeptit/src/j07020/Customer.java
+++ submit/codeptit/src/j07020/Customer.java
@@ -7,6 +7,7 @@ public class Customer {
     private String id, name, gender, dob, address;
     private static int count = 0;
     private static Map<String, Customer> customerMap = new HashMap<>();
+
     public Customer(String name, String gender, String dob, String address) {
         this.name = name;
         this.gender = gender;
@@ -16,15 +17,19 @@ public class Customer {
         this.id = String.format("KH%03d", count);
         customerMap.put(id, this);
     }
+
     public static Map<String, Customer> getCustomerMap() {
         return customerMap;
     }
+
     public String getAddress() {
         return address;
     }
+
     public String getName() {
         return name;
     }
+
     @Override
     public String toString() {
         return String.format("%s %s ", name, address);
