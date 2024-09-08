7514d58298a0828dee29d8ebb09747bab0280c83
duongvu6 <vucongtuanduong6@gmail.com>
2024-09-08 08:20:44 +0000

    update

--- submit/codeptit/src/J07035/J07035.java
+++ submit/codeptit/src/J07035/J07035.java
@@ -8,65 +8,37 @@ public class J07035 {
         // Scanner input2 = new Scanner(new File("J07035/MONHOC.in"));
         // Scanner input3 = new Scanner(new File("J07035/BANGDIEM.in"));
 
-        Scanner input1 = new Scanner(new File("SINHVIEN.in"));
-        Scanner input2 = new Scanner(new File("MONHOC.in"));
-        Scanner input3 = new Scanner(new File("BANGDIEM.in"));
+        // Scanner input1 = new Scanner(new File("SINHVIEN.in"));
+        // Scanner input2 = new Scanner(new File("MONHOC.in"));
+        // Scanner input3 = new Scanner(new File("BANGDIEM.in"));
 
-        int n = Integer.parseInt(input1.nextLine().trim());
-        ArrayList<Student> students = new ArrayList<>();
-        for (int i = 0; i < n; i++) {
-            students.add(new Student(input1.nextLine().trim(), input1.nextLine().trim(), input1.nextLine().trim(), input1.nextLine().trim()));
-        }
-
-        int m = Integer.parseInt(input2.nextLine().trim());
+        int n = Integer.parseInt(input2.nextLine());
         ArrayList<Course> courses = new ArrayList<>();
-        for (int i = 0; i < m; i++) {
-            courses.add(new Course(input2.nextLine().trim(), input2.nextLine().trim(), Integer.parseInt(input2.nextLine().trim())));
+        for (int i = 0; i < n; i++) {
+            courses.add(new Course(input2.nextLine(), input2.nextLine(), Integer.parseInt(input2.nextLine())));
         }
-
-        int p = Integer.parseInt(input3.nextLine().trim());
+        int m = Integer.parseInt(input1.nextLine()) ;
+        ArrayList<Student> students = new ArrayList<>();
+        for (int i = 0; i < m; i++) {
+            students.add(new Student(input1.nextLine(), input1.nextLine(), input1.nextLine(), input1.nextLine()));
+        }
+        int p = Integer.parseInt(input3.nextLine());
         ArrayList<Transcript> transcripts = new ArrayList<>();
         for (int i = 0; i < p; i++) {
-            String studentId = input3.next();
-            String courseId = input3.next();
-            String gpa = input3.next();
-            Student student = new Student();
-            Course course = new Course();
-
-            for (Student st : students) {
-                if (st.getId().equals(studentId)) {
-                    student = st;
-                    break;
-                }
-            }
-            for (Course sj : courses) {
-                if (sj.getId().equals(courseId)) {
-                    course = sj;
-                    break;
-                }
-            }
-            transcripts.add(new Transcript(student, course, gpa));
+            transcripts.add(new Transcript(input3.nextLine()));
         }
-
         Collections.sort(transcripts, new CompareByGpaAndId());
-
-        int k = Integer.parseInt(input3.next());
+        int k = Integer.parseInt(input3.nextLine());
         for (int i = 0; i < k; i++) {
-            String courseId = input3.next();
-            String courseName = "";
-            for (Course cs : courses) {
-                if (cs.getId().equals(courseId)) {
-                    courseName = cs.getName();
-                    break;
-                }
-            }
-            System.out.printf("BANG DIEM MON %s:\n", courseName);
+            String courseId = input3.nextLine();
+            System.out.printf("BANG DIEM MON %s:\n", Course.getCourseMap().get(courseId).getName());
             for (Transcript x : transcripts) {
                 if (x.getCourse().getId().equals(courseId)) {
                     System.out.println(x);
                 }
             }
         }
+
 
         input1.close();
         input2.close();
