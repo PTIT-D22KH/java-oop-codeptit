bb435976f05dfee8d7d08e773b0a4d2e6497969f
duongvu6 <vucongtuanduong6@gmail.com>
2024-09-12 03:04:02 +0000

    update

--- submit/codeptit/src/J02026/J02026.java
+++ submit/codeptit/src/J02026/J02026.java
@@ -1,8 +1,6 @@
 package J02026;
 
 import java.util.Arrays;
-import java.util.Collections;
-import java.util.Comparator;
 import java.util.Scanner;
 
 public class J02026 {
@@ -12,12 +10,12 @@ public class J02026 {
 
     private static void result() {
         int sum = 0;
-        for(int i = 1; i <= n; i++){
+        for (int i = 0; i < n; i++) {
             sum += b[i];
         }
-        if(sum == (n - k)){
-            for(int i = 1; i <= n; i++){
-                if(b[i] == 0)
+        if (sum == (n - k)) {
+            for (int i = 0; i < n; i++) {
+                if (b[i] == 0)
                     System.out.print(a[i] + " ");
             }
             System.out.println("");
@@ -27,7 +25,7 @@ public class J02026 {
     public static void Try(int i) {
         for (int j = 0; j <= 1; j++) {
             b[i] = j;
-            if (i == n) {
+            if (i == n - 1) {
                 result();
             } else {
                 Try(i + 1);
@@ -41,11 +39,11 @@ public class J02026 {
         while (t-- > 0) {
             n = input.nextInt();
             k = input.nextInt();
-            for (int i = 1; i <= n; i++) {
+            for (int i = 0; i < n; i++) {
                 a[i] = input.nextInt();
             }
-            Arrays.sort(a, 1, n + 1);
-            Try(1);
+            Arrays.sort(a, 0, n);
+            Try(0);
         }
         input.close();
     }
