e1c06b651115dc53d3de45b4460ffec20919dca5
duongvu6 <vucongtuanduong6@gmail.com>
2024-09-14 00:33:51 +0000

    add files

--- classwork/classwork/src/ArrayListLearn/ArrayListLearn.java
+++ classwork/classwork/src/ArrayListLearn/ArrayListLearn.java
@@ -1,7 +1,28 @@
 package ArrayListLearn;
-
+import java.util.*;
 public class ArrayListLearn {
     public static void main(String[] args) {
         // Write your code here
+        ArrayList<String> names = new ArrayList<>();
+        names.add("Asa");
+        names.add("Nathan");
+        names.add(1, "Test");
+
+        ArrayList<String> names2 = new ArrayList<>();
+        names2.add("Asada");
+        names2.add("Nathaniel");
+        names2.add(1, "Testing");
+
+        names.addAll(names2);
+        ArrayList<String> names3 = new ArrayList<>();
+        names3.addAll(names);
+        names3.retainAll(names2);
+        for (String x : names3) {
+            System.out.println(x);
+        }
+        System.out.println("------------------");
+        for (String x : names) {
+            System.out.println(x);
+        }
     }
 }
--- classwork/classwork/src/ArrayListLearn/README.md
+++ classwork/classwork/src/ArrayListLearn/README.md
@@ -1,4 +1,8 @@
+
 ## ArrayListLearn
+
+- [ArrayListLearn.class](ArrayListLearn.class)
 - [ArrayListLearn.java](ArrayListLearn.java)
 - [input.txt](input.txt)
 - [output.txt](output.txt)
+- [README.md](README.md)
