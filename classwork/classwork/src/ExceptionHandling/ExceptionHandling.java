37d26602087a15d4239a2fff6687329715a4b64a
duongvu5 <vucongtuanduong5@gmail.com>
2024-09-21 01:43:48 +0000

    add files

---
+++ classwork/classwork/src/ExceptionHandling/ExceptionHandling.java
@@ -0,0 +1,9 @@
+package ExceptionHandling;
+import java.util.*;
+import java.io.*;
+public class ExceptionHandling {
+    public static void main(String[] args) throws FileNotFoundException{
+        // Write your code here
+        Scanner input = new Scanner(new File("ExceptionHandling/vd.txt"));
+    }
+}
---
+++ classwork/classwork/src/ExceptionHandling/README.md
@@ -0,0 +1,4 @@
+## ExceptionHandling
+- [ExceptionHandling.java](ExceptionHandling.java)
+- [input.txt](input.txt)
+- [output.txt](output.txt)
