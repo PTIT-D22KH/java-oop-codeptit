6d66595ab561db0be988afdb4671f421509383ba
duongvu5 <vucongtuanduong5@gmail.com>
2024-09-28 00:31:28 +0000

    add files

---
+++ classwork/classwork/src/IOBinaryFile/IOBinaryFile.java
@@ -0,0 +1,7 @@
+package IOBinaryFile;
+
+public class IOBinaryFile {
+    public static void main(String[] args) {
+        // Write your code here
+    }
+}
---
+++ classwork/classwork/src/IOBinaryFile/README.md
@@ -0,0 +1,4 @@
+## IOBinaryFile
+- [IOBinaryFile.java](IOBinaryFile.java)
+- [input.txt](input.txt)
+- [output.txt](output.txt)
